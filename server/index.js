b37ef436e22c60556a91a7e06fd27f502dc91755
himanshu joshi <sahiljsd@gmailcom>
2026-05-16 17:44:55 +0530

    Fix Express 5 catch-all syntax

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.use('/api/playlists', playlistRoutes)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')))
   
-  app.get('(.*)', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'))
   })
 } else {
